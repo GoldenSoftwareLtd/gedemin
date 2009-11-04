8453694d30ae2564eb2a1f287edd963deebf13f4
Golden Software of Belarus, Ltd. <gs1994@gmail.com>
2009-11-04 14:34:17 +0000

    Изменена передача парамера для конвертации БД в ODS 11.1+, при ресторе БД.

--- Gedemin/IBX/IBServices.pas
+++ Gedemin/IBX/IBServices.pas
@@ -382,6 +382,7 @@ type
     FOptions: TRestoreOptions;
     FPageSize: Integer;
     FPageBuffers: Integer;
+    FFixFssCharacterSet: String;
     procedure SetBackupFile(const Value: TStrings);
     procedure SetDatabaseName(const Value: TStrings);
   protected
@@ -395,6 +396,7 @@ type
     property BackupFile: TStrings read FBackupFile write SetBackupFile;
     property PageSize: Integer read FPageSize write FPageSize default 4096;
     property PageBuffers: Integer read FPageBuffers write FPageBuffers;
+    property FixFssCharacterSet: String read FFixFssCharacterSet write FFixFssCharacterSet;
     property Options : TRestoreOptions read FOptions write FOptions default [CreateNewDB];
   end;
 
@@ -1534,8 +1536,6 @@ begin
     param := param or isc_spb_res_create;
   if (UseAllSpace in Options) then
     param := param or isc_spb_res_use_all_space;
-  if (FixFss in Options) then
-    param := param or isc_spb_res_fix_fss_data or isc_spb_res_fix_fss_metadata;
   Action := isc_action_svc_restore;
   ServiceStartParams  := Char(isc_action_svc_restore);
   ServiceStartAddParam(param, SPBConstantValues[isc_spb_options]);
@@ -1544,6 +1544,12 @@ begin
     ServiceStartAddParam(FPageSize, isc_spb_res_page_size);
   if FPageBuffers > 0 then
     ServiceStartAddParam(FPageBuffers, isc_spb_res_buffers);
+  // Кодовая страница для конвертации БД в ODS 11.1 +
+  if FFixFssCharacterSet <> '' then
+  begin
+    ServiceStartAddParam(FixFssCharacterSet, isc_spb_res_fix_fss_data);
+    ServiceStartAddParam(FixFssCharacterSet, isc_spb_res_fix_fss_metadata);
+  end;
   for i := 0 to FBackupFile.Count - 1 do
   begin
     if (Trim(FBackupFile[i]) = '') then continue;
@@ -1579,6 +1585,7 @@ begin
   FBackupFile := TStringList.Create;
   Include (FOptions, CreateNewDB);
   FPageSize := 4096;
+  FixFssCharacterSet := '';
 end;
 
 destructor TIBRestoreService.Destroy;
