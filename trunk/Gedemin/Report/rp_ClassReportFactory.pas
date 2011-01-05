0d201d44a1f59f184ddb541c59340fe042a70480
Alexander.GoldenSoft@gmail.com <Alexander.GoldenSoft@gmail.com@01dfe959-1d1a-0410-a567-f9ff5baf8fed>
2011-01-05 12:15:01 +0000

    Избавились от построении отчета в отдельной нити, Issue 2067

--- trunk/Gedemin/Report/rp_ClassReportFactory.pas
+++ trunk/Gedemin/Report/rp_ClassReportFactory.pas
@@ -5,45 +5,24 @@ interface
 uses
   Classes, SysUtils, rp_BaseReport_unit, rp_i_ReportBuilder_unit,
   Windows, Forms, {RTF deleted} {rp_StreamRTF, }rp_StreamNone, rp_StreamFR,
-  rp_StreamXFR, rp_StreamGRD, rp_report_const, JclDebug{$IFDEF FR4}, rp_StreamFR4{$ENDIF};
-
-type
-  TrpBuildReportThread = class(TJclDebugThread)
-  private
-    FReportInterface: IgsReportBuilder;
-    FFormShowing: Boolean;
-
-    procedure ShowForm;
-    procedure FreeInterface;
-    procedure DoClearTerminate;
-  protected
-    procedure Execute; override;
-    procedure Proceed;
-  public
-    constructor Create(AnReportInterface: IgsReportBuilder);
-    destructor Destroy; override;
-
-    procedure ClearTerminate;
-  end;   
+  rp_StreamXFR, rp_StreamGRD, rp_report_const {$IFDEF FR4}, rp_StreamFR4{$ENDIF};
 
 type
   TReportFactory = class(TComponent)
   private
-    FThreadList: TList;
     FOnReportEvent: TReportEvent;
     FPrinterName: String;
     FShowProgress: Boolean;
     FFileName: String;
     FExportType: TExportType;
 
-    procedure DoTerminate(Sender: TObject);
     function GetReportInterface(const AnTemplateStructure: TTemplateStructure;
      AnReportResult: TReportResult; const AnParams: Variant;
      const AnBuildDate: TDateTime; const AnPreview: Boolean;
      const AnEventFunction: TrpCustomFunction; const AnReportName: String; const AnBaseQueryList: Variant): IgsReportBuilder;
-    procedure TerminateChildThreads;
     function GetReportEvent: TReportEvent;
     procedure SetReportEvent(Value: TReportEvent);
+    procedure ShowForm(const FReportInterface: IgsReportBuilder);
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
@@ -54,7 +33,6 @@ type
      const AnEventFunction: TrpCustomFunction; const AnReportName: String;
      const AnPrinterName: String; const AnShowProgress: Boolean; const AnBaseQueryList: Variant;
      const AnFileName: String; const AnExportType: TExportType);
-    procedure Clear;
   published
     property OnReportEvent: TReportEvent read GetReportEvent write SetReportEvent;
   end;
@@ -62,7 +40,7 @@ type
 procedure Register;
 
 var
-  ReportIsBuilding: Boolean;   
+  ReportIsBuilding: Boolean;
 
 implementation
 
@@ -79,136 +57,20 @@ uses
   {$ENDIF}
    ;
 
-constructor TrpBuildReportThread.Create(AnReportInterface: IgsReportBuilder);
-begin
-  inherited Create(True);
-
-  FreeOnTerminate := True;
-  FFormShowing := False;
-
-  if AnReportInterface <> nil then
-    FReportInterface := AnReportInterface
-  else
-    Terminate;
-  ReportIsBuilding := True;
-
-  Resume;
-end;
-
-destructor TrpBuildReportThread.Destroy;
-begin
-  ReportIsBuilding := False;
-  inherited Destroy;
-end;
-
-procedure TrpBuildReportThread.Proceed;
-begin
-  FFormShowing := FReportInterface.IsProcessed;
-end;
-
-procedure TrpBuildReportThread.FreeInterface;
-begin
-  FReportInterface := nil;
-end;
-
-procedure TrpBuildReportThread.DoClearTerminate;
-begin
-  OnTerminate := nil;
-  Terminate;
-end;
-
-procedure TrpBuildReportThread.ClearTerminate;
-begin
-  Synchronize(DoClearTerminate);
-end;
-
-procedure TrpBuildReportThread.ShowForm;
-begin
-  if FReportInterface.ExportType <> etNone then
-    FReportInterface.ExportReport(FReportInterface.ExportType, FReportInterface.FileName)
-  else if FReportInterface.Preview then
-    FReportInterface.BuildReport
-  else begin
-    
-  {$IFDEF GEDEMIN_LOCK}
-    if not IsRegisteredCopy then
-    begin       
-      MessageBox(0,
-        'Вы используете незарегистрированную копию программы. Печать невозможна.'#13#10 +
-        'Вы можете выполнить регистрацию вызвав команду Регистрация'#13#10 +
-        'из пункта меню Справка главного окна программы.',
-        'Внимание',
-        MB_OK or MB_ICONEXCLAMATION or MB_TASKMODAL);
-      Exit;
-    end;
-  {$ENDIF}        
-
-    if Assigned(GlobalStorage) and Assigned(IBLogin)
-      and ((GlobalStorage.ReadInteger('Options\Policy',
-        GD_POL_PRINT_ID, GD_POL_PRINT_MASK, False) and IBLogin.InGroup) = 0) then
-    begin
-      MessageBox(0,
-        'Печать документов запрещена текущими настройками политики безопасности.',
-        'Отказано в доступе',
-        MB_OK or MB_ICONHAND or MB_TASKMODAL);
-      Exit;
-    end;
-
-    FReportInterface.PrintReport;
-  end;
-end;
-
-procedure TrpBuildReportThread.Execute;
-begin
-  if Terminated then
-    Exit;
-
-  FFormShowing := True;
-
-  Synchronize(ShowForm);
-  Synchronize(FreeInterface);
-end;
-
 // TReportFactory
 
 constructor TReportFactory.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
 
-  FThreadList := nil;
   FOnReportEvent := nil;
   FFileName := '';
   FExportType := etNone;
-
-  if not (csDesigning in ComponentState) then
-  begin
-    FThreadList := TList.Create;
-  end;
 end;
 
 destructor TReportFactory.Destroy;
 begin
-  if Assigned(FThreadList) then
-  begin
-    TerminateChildThreads;
-    FreeAndNil(FThreadList);
-  end;
-
   inherited Destroy;
-end;
-
-procedure TReportFactory.TerminateChildThreads;
-var
-  I: Integer;
-begin
-  for I := FThreadList.Count - 1 downto 0 do
-  begin
-    TrpBuildReportThread(FThreadList.Items[I]).FreeOnTerminate := False;
-    TrpBuildReportThread(FThreadList.Items[I]).ClearTerminate;
-    TrpBuildReportThread(FThreadList.Items[I]).WaitFor;
-    TrpBuildReportThread(FThreadList.Items[I]).Free;
-  end;
-  FThreadList.Clear;
 end;
 
 function TReportFactory.GetReportInterface(const AnTemplateStructure: TTemplateStructure;
@@ -244,7 +106,6 @@ begin
   else
   begin
     if AnReportResult.IsStreamData then
-//      Result.ReportResult.LoadFromStream(AnReportResult.TempStream)
       Result.ReportResult.AddDataSetList(AnBaseQueryList)
     else
       Result.ReportResult := AnReportResult;
@@ -263,16 +124,7 @@ procedure TReportFactory.CreateReport(const AnTemplateStructure: TTemplateStruct
  const AnPreview: Boolean; const AnEventFunction: TrpCustomFunction; const AnReportName: String;
  const AnPrinterName: String; const AnShowProgress: Boolean; const AnBaseQueryList: Variant;
  const AnFileName: String; const AnExportType: TExportType);
-var
-  I: Integer;
 begin
-  while FThreadList.Count > 0 do
-  begin
-    if Application.Terminated then
-      exit;
-    Application.ProcessMessages;
-  end;
-
   if Assigned(Prn) then
   begin
     if (Prn.Devmode = 0) then
@@ -286,26 +138,8 @@ begin
   FFileName := AnFileName;
   FExportType := AnExportType;
 
-  I := FThreadList.Add(TrpBuildReportThread.Create(GetReportInterface(AnTemplateStructure,
-    AnReportResult, AnParams, AnBuildDate, AnPreview, AnEventFunction, AnReportName, AnBaseQueryList)));
-  TThread(FThreadList.Items[I]).OnTerminate := DoTerminate;
-end;
-
-procedure TReportFactory.Clear;
-begin
-  TerminateChildThreads;
-end;
-
-procedure TReportFactory.DoTerminate(Sender: TObject);
-var
-  I: Integer;
-begin
-  if Assigned(FThreadList) then
-  begin
-    I := FThreadList.IndexOf(Sender);
-    if I > -1 then
-      FThreadList.Delete(I);
-  end;
+  ShowForm(GetReportInterface(AnTemplateStructure,
+    AnReportResult, AnParams, AnBuildDate, AnPreview, AnEventFunction, AnReportName, AnBaseQueryList));
 end;
 
 function TReportFactory.GetReportEvent: TReportEvent;
@@ -321,6 +155,44 @@ end;
 procedure Register;
 begin
   RegisterComponents('gsReport', [TReportFactory]);
+end;
+
+procedure TReportFactory.ShowForm(
+  const FReportInterface: IgsReportBuilder);
+begin
+  if FReportInterface.ExportType <> etNone then
+    FReportInterface.ExportReport(FReportInterface.ExportType, FReportInterface.FileName)
+  else if FReportInterface.Preview then
+    FReportInterface.BuildReport
+  else begin
+
+  {$IFDEF GEDEMIN_LOCK}
+    if not IsRegisteredCopy then
+    begin
+      MessageBox(0,
+        'Вы используете незарегистрированную копию программы. Печать невозможна.'#13#10 +
+        'Вы можете выполнить регистрацию вызвав команду Регистрация'#13#10 +
+        'из пункта меню Справка главного окна программы.',
+        'Внимание',
+        MB_OK or MB_ICONEXCLAMATION or MB_TASKMODAL);
+      Exit;
+    end;
+  {$ENDIF}
+
+    if Assigned(GlobalStorage) and Assigned(IBLogin)
+      and ((GlobalStorage.ReadInteger('Options\Policy',
+        GD_POL_PRINT_ID, GD_POL_PRINT_MASK, False) and IBLogin.InGroup) = 0) then
+    begin
+      MessageBox(0,
+        'Печать документов запрещена текущими настройками политики безопасности.',
+        'Отказано в доступе',
+        MB_OK or MB_ICONHAND or MB_TASKMODAL);
+      Exit;
+    end;
+
+    FReportInterface.PrintReport;
+  end;
+
 end;
 
 end.
