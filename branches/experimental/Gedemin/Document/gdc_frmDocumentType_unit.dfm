64db3654cea238c67b802bc2446b771873cc9963
pzholnerovich <pzholnerovich@01dfe959-1d1a-0410-a567-f9ff5baf8fed>
2015-04-02 12:42:40 +0000

    подправлнена форма типовых документов, выпадающий список не совсем корректкн, но это не имеет отношение к проблеме dfm

--- branches/experimental/Gedemin/Document/gdc_frmDocumentType_unit.dfm
+++ branches/experimental/Gedemin/Document/gdc_frmDocumentType_unit.dfm
@@ -1,80 +1,117 @@
 inherited gdc_frmDocumentType: Tgdc_frmDocumentType
-  Left = 312
-  Top = 88
-  Width = 775
-  Height = 539
+  Left = 365
+  Top = 169
+  Width = 795
+  Height = 542
   Caption = 'Типовые документы'
   PixelsPerInch = 96
   TextHeight = 13
   inherited sbMain: TStatusBar
-    Top = 482
-    Width = 759
+    Top = 485
+    Width = 779
   end
   inherited TBDockTop: TTBDock
-    Width = 759
+    Width = 779
+    inherited tbMainToolbar: TTBToolbar
+      object TBSubmenuItem1: TTBSubmenuItem [1]
+        Action = actNew
+        DropdownCombo = True
+        object TBItem1: TTBItem
+          Action = actNew
+        end
+        object TBItem2: TTBItem
+          Action = actNewSub
+        end
+      end
+    end
+    inherited tbMainInvariant: TTBToolbar
+      Left = 309
+    end
   end
   inherited TBDockLeft: TTBDock
-    Height = 422
+    Height = 425
   end
   inherited TBDockRight: TTBDock
-    Left = 750
-    Height = 422
+    Left = 770
+    Height = 425
   end
   inherited TBDockBottom: TTBDock
-    Top = 473
-    Width = 759
+    Top = 476
+    Width = 779
   end
   inherited pnlWorkArea: TPanel
-    Width = 741
-    Height = 422
+    Width = 761
+    Height = 425
     inherited sMasterDetail: TSplitter
-      Height = 317
+      Height = 320
     end
     inherited spChoose: TSplitter
-      Top = 317
-      Width = 741
+      Top = 320
+      Width = 761
     end
     inherited pnlMain: TPanel
-      Height = 317
+      Height = 320
       inherited pnlSearchMain: TPanel
-        Height = 317
+        Height = 320
         inherited sbSearchMain: TScrollBox
-          Height = 290
+          Height = 293
         end
       end
       inherited tvGroup: TgsDBTreeView
-        Height = 317
+        Height = 320
+        Images = dmImages.ilTree
         OnGetImageIndex = tvGroupGetImageIndex
       end
     end
     inherited pnChoose: TPanel
-      Top = 323
-      Width = 741
+      Top = 326
+      Width = 761
       inherited pnButtonChoose: TPanel
-        Left = 636
+        Left = 656
       end
       inherited ibgrChoose: TgsIBGrid
-        Width = 636
+        Width = 656
       end
       inherited pnlChooseCaption: TPanel
-        Width = 741
+        Width = 761
       end
     end
     inherited pnlDetail: TPanel
-      Width = 569
-      Height = 317
+      Width = 589
+      Height = 320
       inherited TBDockDetail: TTBDock
-        Width = 569
+        Width = 589
+        inherited tbDetailToolbar: TTBToolbar
+          object tbsmNew: TTBSubmenuItem [0]
+            Caption = 'Добавить'
+            DropdownCombo = True
+            Hint = 'Добавить документ'
+            ImageIndex = 0
+            OnClick = tbsmNewClick
+            object tbiAddUserDoc: TTBItem
+              Action = actAddUserDoc
+            end
+            object tbiAddInvDocument: TTBItem
+              Action = actAddInvDocument
+            end
+            object tbiAddInvPriceList: TTBItem
+              Action = actAddInvPriceList
+            end
+          end
+        end
+        inherited tbDetailCustom: TTBToolbar
+          Left = 286
+        end
       end
       inherited pnlSearchDetail: TPanel
-        Height = 291
+        Height = 294
         inherited sbSearchDetail: TScrollBox
-          Height = 264
+          Height = 267
         end
       end
       inherited ibgrDetail: TgsIBGrid
-        Width = 409
-        Height = 291
+        Width = 429
+        Height = 294
       end
     end
   end
@@ -85,6 +122,36 @@ inherited gdc_frmDocumentType: Tgdc_frmDocumentType
       Caption = 'Добавить ветвь...'
       Hint = 'Добавить ветвь...'
       ImageIndex = 0
+      OnExecute = actNewSubExecute
+      OnUpdate = actNewSubUpdate
+    end
+    inherited actDetailNew: TAction
+      Visible = False
+      OnExecute = actAddUserDocExecute
+    end
+    object actAddUserDoc: TAction
+      Category = 'Detail'
+      Caption = 'Добавить документ пользователя'
+      Hint = 'Добавить документ пользователя'
+      ImageIndex = 0
+      OnExecute = actAddUserDocExecute
+      OnUpdate = actAddUserDocUpdate
+    end
+    object actAddInvDocument: TAction
+      Category = 'Detail'
+      Caption = 'Добавить складской документ'
+      Hint = 'Добавить складской документ'
+      ImageIndex = 0
+      OnExecute = actAddInvDocumentExecute
+      OnUpdate = actAddInvDocumentUpdate
+    end
+    object actAddInvPriceList: TAction
+      Category = 'Detail'
+      Caption = 'Добавить прайс-лист'
+      Hint = 'Добавить прайс-лист'
+      ImageIndex = 0
+      OnExecute = actAddInvPriceListExecute
+      OnUpdate = actAddInvPriceListUpdate
     end
   end
   inherited pmMain: TPopupMenu
@@ -112,7 +179,7 @@ inherited gdc_frmDocumentType: Tgdc_frmDocumentType
     Top = 140
   end
   object gdcBaseDocumentType: TgdcBaseDocumentType
-    Left = 73
-    Top = 206
+    Left = 72
+    Top = 128
   end
 end
--- branches/experimental/Gedemin/Document/gdc_frmDocumentType_unit.pas
+++ branches/experimental/Gedemin/Document/gdc_frmDocumentType_unit.pas
@@ -7,20 +7,46 @@ uses
   gdc_frmMDVTree_unit, Db, Menus, ActnList, Grids, DBGrids, gsDBGrid,
   gsIBGrid, ComCtrls, gsDBTreeView, ToolWin, ExtCtrls, TB2Item, TB2Dock,
   TB2Toolbar, gdcClasses, IBCustomDataSet, gdcBase, gdcTree, StdCtrls,
-  gd_MacrosMenu, dmImages_unit, Contnrs;
+  gd_MacrosMenu, dmImages_unit;
 
 type
   Tgdc_frmDocumentType = class(Tgdc_frmMDVTree)
     gdcDocumentType: TgdcDocumentType;
     actNewSub: TAction;
+    TBSubmenuItem1: TTBSubmenuItem;
+    TBItem1: TTBItem;
+    TBItem2: TTBItem;
+    tbsmNew: TTBSubmenuItem;
+    actAddUserDoc: TAction;
+    actAddInvDocument: TAction;
+    actAddInvPriceList: TAction;
+    tbiAddInvPriceList: TTBItem;
+    tbiAddInvDocument: TTBItem;
+    tbiAddUserDoc: TTBItem;
     gdcBaseDocumentType: TgdcBaseDocumentType;
     procedure FormCreate(Sender: TObject);
-    procedure tvGroupGetImageIndex(Sender: TObject; Node: TTreeNode);
+    procedure actNewSubExecute(Sender: TObject);
+    procedure actNewSubUpdate(Sender: TObject);
     procedure actDetailNewExecute(Sender: TObject);
+    procedure actAddUserDocExecute(Sender: TObject);
+    procedure actAddInvDocumentExecute(Sender: TObject);
+    procedure actAddInvPriceListExecute(Sender: TObject);
+    procedure actAddInvDocumentUpdate(Sender: TObject);
+    procedure actAddInvPriceListUpdate(Sender: TObject);
+    procedure actAddUserDocUpdate(Sender: TObject);
+    procedure tbsmNewClick(Sender: TObject);
+    procedure actNewExecute(Sender: TObject);
+    procedure tvGroupGetImageIndex(Sender: TObject; Node: TTreeNode);
   private
+
+  protected
+    procedure SetGdcObject(const Value: TgdcBase); override;
+
+    procedure SetgdcDetailObject(const Value: TgdcBase); override;
 
   public
     class function CreateAndAssign(AnOwner: TComponent): TForm; override;
+
   end;
 
 var
@@ -33,6 +59,35 @@ implementation
 uses gdcBaseInterface,  gd_ClassList, gdcInvPriceList_unit, gdcInvDocument_unit;
 
 { Tgdc_frmDocumentType }
+
+procedure Tgdc_frmDocumentType.SetgdcDetailObject(const Value: TgdcBase);
+begin
+  inherited;
+
+  tbsmNew.Clear;
+  tbsmNew.DropdownCombo := gdClassList.Get(TgdBaseEntry, gdcDetailObject.ClassName,
+    gdcDetailObject.SubType).Count > 0;
+  tbsmNew.OnPopup := tbiDetailNewPopup;
+
+  actDetailNew.Visible := True;
+  actDetailNew.OnExecute := actDetailNewExecute;
+
+  tbiDetailNew.Visible := False;
+
+  tbsmNew.Action := actDetailNew;
+end;
+
+procedure Tgdc_frmDocumentType.SetGdcObject(const Value: TgdcBase);
+begin
+  inherited;
+
+  tbiNew.Visible := False;
+
+  TBSubmenuItem1.Clear;
+  TBSubmenuItem1.DropdownCombo := gdClassList.Get(TgdBaseEntry, gdcObject.ClassName,
+    gdcObject.SubType).Count > 0;
+  TBSubmenuItem1.OnPopup := tbiNewPopup;
+end;
 
 class function Tgdc_frmDocumentType.CreateAndAssign(
   AnOwner: TComponent): TForm;
@@ -50,17 +105,14 @@ begin
   inherited;
 end;
 
-procedure Tgdc_frmDocumentType.tvGroupGetImageIndex(Sender: TObject;
-  Node: TTreeNode);
-var
-  V: Variant;
+procedure Tgdc_frmDocumentType.actNewSubExecute(Sender: TObject);
 begin
-  V := gdcObject.GetFieldValueForID(Integer(Node.Data), 'documenttype');
-  if (VarType(V) = varString) and (V = 'D') then
-    Node.ImageIndex := 3
-  else
-    Node.ImageIndex := 0;
-  Node.SelectedIndex := Node.ImageIndex;
+  // Не удалять!!! Нужен для подджки dfm до наследования!!!
+end;
+
+procedure Tgdc_frmDocumentType.actNewSubUpdate(Sender: TObject);
+begin
+// Не удалять!!! Нужен для подджки dfm до наследования!!!
 end;
 
 procedure Tgdc_frmDocumentType.actDetailNewExecute(Sender: TObject);
@@ -83,6 +135,59 @@ begin
       else
         gdcDetailObject.CreateDialog(TgdcUserDocumentType);
   end;
+end;
+
+procedure Tgdc_frmDocumentType.actAddUserDocExecute(Sender: TObject);
+begin
+  // Не удалять!!! Нужен для подджки dfm до наследования!!!
+end;
+
+procedure Tgdc_frmDocumentType.actAddInvDocumentExecute(Sender: TObject);
+begin
+  // Не удалять!!! Нужен для подджки dfm до наследования!!!
+end;
+
+procedure Tgdc_frmDocumentType.actAddInvPriceListExecute(Sender: TObject);
+begin
+  // Не удалять!!! Нужен для подджки dfm до наследования!!!
+end;
+
+procedure Tgdc_frmDocumentType.actAddInvDocumentUpdate(Sender: TObject);
+begin
+  // Не удалять!!! Нужен для подджки dfm до наследования!!!
+end;
+
+procedure Tgdc_frmDocumentType.actAddInvPriceListUpdate(Sender: TObject);
+begin
+  // Не удалять!!! Нужен для подджки dfm до наследования!!!
+end;
+
+procedure Tgdc_frmDocumentType.actAddUserDocUpdate(Sender: TObject);
+begin
+  // Не удалять!!! Нужен для подджки dfm до наследования!!!
+end;
+
+procedure Tgdc_frmDocumentType.tbsmNewClick(Sender: TObject);
+begin
+  // Не удалять!!! Нужен для подджки dfm до наследования!!!
+end;
+
+procedure Tgdc_frmDocumentType.actNewExecute(Sender: TObject);
+begin
+  gdcObject.CreateDialog(TgdcDocumentBranch);
+end;
+
+procedure Tgdc_frmDocumentType.tvGroupGetImageIndex(Sender: TObject;
+  Node: TTreeNode);
+var
+  V: Variant;
+begin
+  V := gdcObject.GetFieldValueForID(Integer(Node.Data), 'documenttype');
+  if (VarType(V) = varString) and (V = 'D') then
+    Node.ImageIndex := 3
+  else
+    Node.ImageIndex := 0;
+  Node.SelectedIndex := Node.ImageIndex;
 end;
 
 initialization
