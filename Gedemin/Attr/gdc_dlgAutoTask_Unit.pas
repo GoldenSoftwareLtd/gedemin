8e15ecd41e6e8f9e063da22ff7d44c09394c5b67
pzholnerovich <PZholnerovich@gmail.com>
2015-04-29 13:21:49 +0000

    Дизайн диалоговой формы

--- Gedemin/Attr/gdc_dlgAutoTask_Unit.pas
+++ Gedemin/Attr/gdc_dlgAutoTask_Unit.pas
@@ -4,15 +4,43 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  gdc_dlgTR_unit, IBDatabase, Menus, Db, ActnList, StdCtrls, Mask, DBCtrls;
+  gdc_dlgTR_unit, IBDatabase, Menus, Db, ActnList, StdCtrls, Mask, DBCtrls,
+  gsIBLookupComboBox, ExtCtrls, xDateEdits;
 
 type
   Tgdc_dlgAutoTask = class(Tgdc_dlgTR)
-    DBEdit1: TDBEdit;
+    dbedName: TDBEdit;
+    lbName: TLabel;
+    lbDescription: TLabel;
+    dbmDescription: TDBMemo;
+    lbUser: TLabel;
+    iblkupUser: TgsIBLookupComboBox;
+    gbTime: TGroupBox;
+    lbStartTime: TLabel;
+    xDateDBEdit2: TxDateDBEdit;
+    lbEndTime: TLabel;
+    xDateDBEdit3: TxDateDBEdit;
+    gbSettings: TGroupBox;
+    rbSingle: TRadioButton;
+    xdbeExactDate: TxDateDBEdit;
+    rbMonthly: TRadioButton;
+    dbcbMonthly: TDBComboBox;
+    rbWeekly: TRadioButton;
+    dbcbWeekly: TDBComboBox;
+    lbMonthly: TLabel;
+    lbWeekly: TLabel;
+    gbType: TGroupBox;
+    RadioButton1: TRadioButton;
+    iblkupFunction: TgsIBLookupComboBox;
+    rbCmdLine: TRadioButton;
+    dbeCmdLine: TDBEdit;
+    rbBackupFile: TRadioButton;
+    dbeBackupFile: TDBEdit;
   private
     { Private declarations }
   public
-    { Public declarations }
+    procedure SetupRecord; override;
+    procedure BeforePost; override;
   end;
 
 var
@@ -24,6 +52,101 @@ implementation
 
 uses
   gd_ClassList;
+
+procedure Tgdc_dlgAutoTask.SetupRecord;
+var
+  {@UNFOLD MACRO INH_CRFORM_PARAMS()}
+  {M}
+  {M}  Params, LResult: Variant;
+  {M}  tmpStrings: TStackStrings;
+  {END MACRO}
+begin
+  {@UNFOLD MACRO INH_CRFORM_WITHOUTPARAMS('TGDC_DLGAUTOTASK', 'SETUPRECORD', KEYSETUPRECORD)}
+  {M}  try
+  {M}    if Assigned(gdcMethodControl) and Assigned(ClassMethodAssoc) then
+  {M}    begin
+  {M}      SetFirstMethodAssoc('TGDC_DLGAUTOTASK', KEYSETUPRECORD);
+  {M}      tmpStrings := TStackStrings(ClassMethodAssoc.IntByKey[KEYSETUPRECORD]);
+  {M}      if (tmpStrings = nil) or (tmpStrings.IndexOf('TGDC_DLGAUTOTASK') = -1) then
+  {M}      begin
+  {M}        Params := VarArrayOf([GetGdcInterface(Self)]);
+  {M}        if gdcMethodControl.ExecuteMethodNew(ClassMethodAssoc, Self, 'TGDC_DLGAUTOTASK',
+  {M}          'SETUPRECORD', KEYSETUPRECORD, Params, LResult) then exit;
+  {M}      end else
+  {M}        if tmpStrings.LastClass.gdClassName <> 'TGDC_DLGAUTOTASK' then
+  {M}        begin
+  {M}          Inherited;
+  {M}          Exit;
+  {M}        end;
+  {M}    end;
+  {END MACRO}
+
+  inherited;
+
+  {iblkupFunction.Enabled := False;
+  dbeCmdLine.Enabled := False;
+  dbeBackupFile.Enabled := False;
+
+  if gdcObject.FieldByName('functionkey').AsInteger > 0 then
+  begin
+    rgRun.ItemIndex := 0;
+    iblkupFunction.Enabled := True;
+  end
+  else if gdcObject.FieldByName('cmdline').AsString > '' then
+  begin
+    rgRun.ItemIndex := 1;
+    dbeCmdLine.Enabled := True;
+  end
+  else if gdcObject.FieldByName('backupfile').AsString > '' then
+  begin
+    rgRun.ItemIndex := 2;
+    dbeBackupFile.Enabled := True;
+  end;}
+
+  {@UNFOLD MACRO INH_CRFORM_FINALLY('TGDC_DLGEXPLORER', 'SETUPRECORD', KEYSETUPRECORD)}
+  {M}finally
+  {M}  if Assigned(gdcMethodControl) and Assigned(ClassMethodAssoc) then
+  {M}    ClearMacrosStack('TGDC_DLGEXPLORER', 'SETUPRECORD', KEYSETUPRECORD);
+  {M}end;
+  {END MACRO}
+end;
+
+procedure Tgdc_dlgAutoTask.BeforePost;
+var
+  {@UNFOLD MACRO INH_CRFORM_PARAMS()}
+  {M}  Params, LResult: Variant;
+  {M}  tmpStrings: TStackStrings;
+  {END MACRO}
+begin
+  {@UNFOLD MACRO INH_CRFORM_WITHOUTPARAMS('TGDC_DLGAUTOTASK', 'BEFOREPOST', KEYBEFOREPOST)}
+  {M}  try
+  {M}    if Assigned(gdcMethodControl) and Assigned(ClassMethodAssoc) then
+  {M}    begin
+  {M}      SetFirstMethodAssoc('TGDC_DLGAUTOTASK', KEYBEFOREPOST);
+  {M}      tmpStrings := TStackStrings(ClassMethodAssoc.IntByKey[KEYBEFOREPOST]);
+  {M}      if (tmpStrings = nil) or (tmpStrings.IndexOf('TGDC_DLGAUTOTASK') = -1) then
+  {M}      begin
+  {M}        Params := VarArrayOf([GetGdcInterface(Self)]);
+  {M}        if gdcMethodControl.ExecuteMethodNew(ClassMethodAssoc, Self, 'TGDC_DLGAUTOTASK',
+  {M}          'BEFOREPOST', KEYBEFOREPOST, Params, LResult) then exit;
+  {M}      end else
+  {M}        if tmpStrings.LastClass.gdClassName <> 'TGDC_DLGAUTOTASK' then
+  {M}        begin
+  {M}          Inherited;
+  {M}          Exit;
+  {M}        end;
+  {M}    end;
+  {END MACRO}
+
+  inherited;
+
+  {@UNFOLD MACRO INH_CRFORM_FINALLY('TGDC_DLGAUTOTASK', 'BEFOREPOST', KEYBEFOREPOST)}
+  {M}finally
+  {M}  if Assigned(gdcMethodControl) and Assigned(ClassMethodAssoc) then
+  {M}    ClearMacrosStack('TGDC_DLGAUTOTASK', 'BEFOREPOST', KEYBEFOREPOST);
+  {M}end;
+  {END MACRO}
+end;
 
 initialization
   RegisterFrmClass(Tgdc_dlgAutoTask);
