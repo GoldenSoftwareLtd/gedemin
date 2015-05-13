f8cf5637e6a0c6719985e64187dd575617b3e967
pzholnerovich <PZholnerovich@gmail.com>
2015-05-13 09:12:40 +0000

    Issue 3582:	Создание/удаление TgdcDelphiObject при каждом вызове диалогового окна

--- Gedemin/Component/Repository/gdc_dlgG_unit.pas
+++ Gedemin/Component/Repository/gdc_dlgG_unit.pas
@@ -316,7 +316,7 @@ implementation
 uses
   IB, IBSQL, IBErrorCodes, gdcBaseInterface, gd_createable_form,
   IBCustomDataSet, TypInfo, gd_directories_const, Storages, gd_ClassList,
-  dmImages_unit, evt_i_Base, jclStrings, at_frmSQLProcess, gsStorage_CompPath,
+  dmImages_unit, evt_Base, evt_i_Base, jclStrings, at_frmSQLProcess, gsStorage_CompPath,
   gd_security, prp_methods, Gedemin_TLB, gsStorage, gdcUser, at_classes,
   DBCtrls, at_AddToSetting, gdcClasses, DBGrids, gdcJournal, gdHelp_Interface,
   gdcDelphiObject
@@ -1824,6 +1824,7 @@ end;
 constructor Tgdc_dlgG.Create(AnOwner: TComponent);
 var
   gdcDelphiObject: TgdcDelphiObject;
+  EvtCtrl: TEventControl;
 begin
   inherited;
   ErrorAction := False;
@@ -1831,12 +1832,21 @@ begin
   FAlreadyRestory := False;
   FAppliedID := -1;
 
-  gdcDelphiObject := TgdcDelphiObject.Create(nil);
-  try
-    gdcDelphiObject.AddObject(Self);
-    gdcDelphiObject.Close;
-  finally
-    gdcDelphiObject.Free;
+
+  EvtCtrl := nil;
+
+  if Assigned(EventControl) then
+    EvtCtrl:= EventControl.Get_Self as TEventControl;
+
+  if (EvtCtrl = nil) or (EvtCtrl.EventObjectList.FindObject(Self.InitialName) = nil) then
+  begin
+    gdcDelphiObject := TgdcDelphiObject.Create(nil);
+    try
+      gdcDelphiObject.AddObject(Self);
+      gdcDelphiObject.Close;
+    finally
+      gdcDelphiObject.Free;
+    end;
   end;
 end;
 
