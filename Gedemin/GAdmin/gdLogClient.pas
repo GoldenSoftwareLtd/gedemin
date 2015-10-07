89e1ffab7ccfcb37fce3f41c1e870beba0a6f138
iamchrisep <christina.popenko@gmail.com>
2015-10-07 10:36:53 +0300

    Начинаем делать TCP клиент.

--- Gedemin/GAdmin/gdLogClient.pas
+++ Gedemin/GAdmin/gdLogClient.pas
@@ -3,30 +3,38 @@ unit gdLogClient;
 interface
 
 uses
-  Classes, Dialogs, Windows, gdMessagedThread, IdTCPConnection, IdTCPClient;
+  Classes, Dialogs, Windows, SyncObjs, gdMessagedThread, IdTCPConnection, IdTCPClient;
+
+const
+  MaxBufferSize = 1024;
+  MaxMsgLength = 255;
 
 type
+  TLogRecord = record
+    DT: TDateTime;
+    Msg: String[MaxMsgLength];
+  end;
+
   TgdLogClient = class(TgdMessagedThread)
   private
-    { Private declarations }
-    //FHost: String;
-    //FPort: Integer;
-    //FMesS: String;
+    FBuffer: array[0..MaxBufferSize - 1] of TLogRecord;
+    FStart, FEnd: Integer;
+    FBufferCS: TCriticalSection;
 
   protected
     TCPClient: TIdTCPClient;
     function ProcessMessage(var Msg: TMsg): Boolean; override;
-
-    //procedure DoWorkLog;
-    //procedure DoWorkClient;
-    //procedure ShowMesS;
+    procedure DoWorkClient;
 
   public
     constructor Create;
     destructor Destroy; override;
-    
+
     procedure Start;
     procedure Finish;
+    procedure Log(const AMsg: String);
+    procedure Connect;
+    procedure Disconnect;
   end;
 
 var
@@ -41,11 +49,15 @@ constructor TgdLogClient.Create;
 begin
   inherited Create(True);
   Priority := tpLowest;
+  FBufferCS := TCriticalSection.Create;
 end;
 
 destructor TgdLogClient.Destroy;
 begin
   inherited;
+  FBufferCS.Free;
+  if TCPClient <> nil then
+    TCPClient.Free;
 end;
 
 function TgdLogClient.ProcessMessage(var Msg: TMsg): Boolean;
@@ -56,11 +68,31 @@ begin
 
     WM_LOG_INIT:
     begin
+
     end;
 
     WM_LOG_DONE:
     begin
-    end
+
+    end;
+
+    WM_LOG_PROCESS_REC:
+    begin
+      FBufferCS.Enter;
+      try
+        while FStart <> FEnd do
+        begin
+          // FBbuffer[FStart] -- log record for processing
+
+          if FStart = MaxBufferSize - 1 then
+            FStart := 0
+          else
+            Inc(FStart);
+        end;
+      finally
+        FBufferCS.Leave;
+      end;
+    end;
 
   else
     Result := False;
@@ -77,53 +109,53 @@ begin
   PostMsg(WM_LOG_DONE);
 end;
 
-{
-procedure TgdLogClient.DoWorkLog;
+procedure TgdLogClient.Log(const AMsg: String);
 begin
-  ShowMessage('gdLog run');
+  FBufferCS.Enter;
+  try
+    if (FEnd = MaxBufferSize) and (FStart > 0) then
+      FEnd := 0;
 
-  // Нужно задать реальный хост и порт сервера
-  FHost := '127.0.0.1';
-  FPort := 7070;
+    if (FEnd < MaxBufferSize) or (FEnd < FStart) then
+    begin
+      FBuffer[FEnd].DT := Now;
+      FBuffer[FEnd].Msg := Copy(AMsg, 1, MaxMsgLength);
+      Inc(FEnd);
+    end;
+
+  finally
+    FBufferCS.Leave;
+  end;
+  DoWorkClient;
+  PostMsg(WM_LOG_PROCESS_REC);
+end;
+
+procedure TgdLogClient.Connect;
+begin
+  if not TCPClient.Connected then
+  begin
+    TCPClient.Host := '127.0.0.1';
+    TCPClient.Port := 7070;
+    TCPClient.Connect;
+    ShowMessage('Connect');
+  end;
+end;
+
+procedure TgdLogClient.Disconnect;
+begin
+  if TCPClient.Connected then
+    TCPClient.Disconnect;
 end;
 
 procedure TgdLogClient.DoWorkClient;
 begin
-  try
     TCPClient := TIdTCPClient.Create(nil);
-    TCPClient.Host := FHost;
-    TCPClient.Port := FPort;
-    while True do
-    begin
-      if Terminated then Exit;
-      try
-        Connect;
-          try
-            if Terminated then Exit;
-            FMesS := ReadLn;
-            Synchronize(ShowMesS);
-          finally
-            Disconnect;
-          end; // try finally
-        except
-          Exit;
-          //on E: Exception do MessageBox(0, PChar('Ошибка при подключении к серверу: ' + E.Message), 'Ошибка', MB_OK or MB_TASKMODAL or MB_ICONHAND);
-        end; // try except
-        if Terminated then Exit;
-        sleep(3000);
-      end; // with
-    end; // while True
-  except
-    //
-    Exit;
-  end; // try except
+    Connect;
+    TCPClient.WriteLn('Message');
+    ShowMessage(TCPClient.ReadLn);
+    Disconnect;
 end;
 
-procedure TgdLogClient.ShowMesS;
-begin
-  ShowMessage(FMesS);
-end;
-}
 
 initialization
   gdLog := TgdLogClient.Create;
