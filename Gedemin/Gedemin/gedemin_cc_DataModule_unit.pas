afe4288debc8a60f0dc65b2bc5a3446f319b04e3
iamchrisep <christina.popenko@gmail.com>
2015-10-20 14:36:09 +0300

    Запись/выборка тестовая таблица
    Signed-off-by: iamchrisep <christina.popenko@gmail.com>

--- Gedemin/Gedemin/gedemin_cc_DataModule_unit.pas
+++ Gedemin/Gedemin/gedemin_cc_DataModule_unit.pas
@@ -7,6 +7,8 @@ uses
   Db, IBDatabase, IBCustomDataSet, IBSQL;
 
 const
+  MaxBufferSize = 1024;
+
   CrGLog =
     'CREATE SEQUENCE gd_g_log;';
 
@@ -62,8 +64,34 @@ const
     '  floati DOUBLE PRECISION, '#13#10 +
     '  CONSTRAINT gd_pk_sql_param PRIMARY KEY (logkey, param))';
 
+  CrTrLogDB =
+    'CREATE TRIGGER gd_t_log_db FOR gd_log_db '#13#10 +
+    'ACTIVE BEFORE INSERT POSITION 0 '#13#10 +
+    'AS '#13#10 +
+    'BEGIN '#13#10 +
+    '  IF (NEW.id IS NULL) THEN '#13#10 +
+    '    NEW.id = GEN_ID(gd_g_log, 1); '#13#10 +
+    'END;';
+
   CrTrLog =
     'CREATE TRIGGER gd_t_log FOR gd_log '#13#10 +
+    'ACTIVE BEFORE INSERT POSITION 0 '#13#10 +
+    'AS '#13#10 +
+    'BEGIN '#13#10 +
+    '  IF (NEW.id IS NULL) THEN '#13#10 +
+    '    NEW.id = GEN_ID(gd_g_log, 1); '#13#10 +
+    'END;';
+
+  CrTTest =
+    'CREATE TABLE test ( '#13#10 +
+    '  id INTEGER NOT NULL, '#13#10 +
+    '  ts TIMESTAMP NOT NULL, '#13#10 +
+    '  host_name VARCHAR(20), '#13#10 +
+    '  data BLOB SUB_TYPE 1, '#13#10 +
+    '  CONSTRAINT pk_test PRIMARY KEY (id))';
+  
+  CrTrTest =
+    'CREATE TRIGGER gd_t_test FOR test '#13#10 +
     'ACTIVE BEFORE INSERT POSITION 0 '#13#10 +
     'AS '#13#10 +
     'BEGIN '#13#10 +
@@ -79,7 +107,9 @@ type
     q: TIBSQL;
     DSrc: TDataSource;
     procedure DataModuleCreate(Sender: TObject);
-
+    procedure InsertDB;
+    procedure UpdateGrid;
+    procedure IBDSGetText (Sender: TField; var Text: String; DisplayText: Boolean);
   public
     procedure CheckDB;
   end;
@@ -118,7 +148,9 @@ begin
     IBDB.Open;
     IBDB.Connected := True;
 
-    IBTr.StartTransaction;
+    if not IBTr.Active then
+      IBTr.StartTransaction;
+
     try
       q.SQL.Text := CrGLog;
       q.ExecQuery;
@@ -135,8 +167,17 @@ begin
       q.SQL.Text := CrTSQLParam;  // int -> inti, float -> floati, иначе ошибка SQL
       q.ExecQuery;
 
+      //q.SQL.Text := CrTrLogDB;
+      //q.ExecQuery;
+
       //q.SQL.Text := CrTrLog;
       //q.ExecQuery;
+
+      q.SQL.Text := CrTTest;
+      q.ExecQuery;
+
+      q.SQL.Text := CrTrTest;
+      q.ExecQuery;
 
       IBTr.Commit;
     except
@@ -154,6 +195,60 @@ begin
   CheckDB;
 end;
 
+procedure TDM.InsertDB;
+begin
+  while ccTCPServer.FStart <> ccTCPServer.FEnd do
+  begin
+    if IBDS.Active then
+      IBDS.Close;
 
+    if not IBTr.Active then
+      IBTr.StartTransaction;
+
+    try
+      {q.SQL.Text := 'INSERT INTO test(ts, host_name, data) ' +
+                    'VALUES (''' + (DateTimeToStr(ccTCPServer.FBuffer[ccTCPServer.FStart].DT)) +
+                    ''', ''' + ccTCPServer.FBuffer[ccTCPServer.FStart].ClientName +
+                    ''', ''' + ccTCPServer.FBuffer[ccTCPServer.FStart].Msg + ''')';
+      q.ExecQuery;}
+
+      IBDS.SelectSQL.Text := 'SELECT ts, host_name, data FROM test';
+
+      IBDS.InsertSQL.Text := 'INSERT INTO test(ts, host_name, data) ' +
+                             'VALUES (:ts, :host_name, :data)';
+
+      IBDS.Open;
+      IBDS.Insert;
+
+      IBDS.FieldByName('ts').AsDateTime := ccTCPServer.FBuffer[ccTCPServer.FStart].DT;
+      IBDS.FieldByName('host_name').AsString := ccTCPServer.FBuffer[ccTCPServer.FStart].ClientName;
+      IBDS.FieldByName('data').AsString := ccTCPServer.FBuffer[ccTCPServer.FStart].Msg;
+
+      IBDS.Post;
+
+      IBTr.Commit;
+
+      if ccTCPServer.FStart = MaxBufferSize - 1 then
+        ccTCPServer.FStart := 0
+      else
+        Inc(ccTCPServer.FStart);
+    except
+      IBTr.Rollback;
+    end;
+  end;
+end;
+
+procedure TDM.UpdateGrid;
+begin
+  IBDS.Open;
+  IBDS.FieldByName('data').OnGetText := IBDSGetText;
+  frm_gedemin_cc_main.DBGr.Refresh;
+end;
+
+procedure TDM.IBDSGetText (Sender: TField;
+var Text: String; DisplayText: Boolean);
+begin
+  Text := Sender.AsString;
+end;
 
 end.
